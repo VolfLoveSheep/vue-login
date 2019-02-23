3325aeb03cfa29838e660a594235d60bb06985c8
Your Name <you@example.com>
2019-02-23 15:32:44 +0800

    登录界面完成

---
+++ src/pages/signIn/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>登录</h1>
+    <el-form ref="form"  label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="usrname"></el-input>
+      </el-form-item>
+    <el-form-item label="密码">
+        <el-input v-model="password"></el-input>
+      </el-form-item>
+    
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+       
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            usrname:'',
+            password:''
+        }
+    },
+    methods:{
+      onSubmit(){
+        console.log('123');
+        
+      }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+

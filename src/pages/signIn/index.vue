28cd30653e2f6694f5d2bcbde7ca5fa702c25d88
VolfLoveSheep <web23xjj@163.com>
2019-02-23 16:28:05 +0800

    登录

--- src/pages/signIn/index.vue
+++ src/pages/signIn/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>登录</h1>
-    <el-form ref="form"  label-width="80px">
+    <el-form   label-width="80px">
       <el-form-item label="用户名">
         <el-input v-model="usrname"></el-input>
       </el-form-item>
